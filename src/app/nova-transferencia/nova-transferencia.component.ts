cf00968d9893d463cfc5dbd6ed83deacecb38a2e
Jean Marcos de Souza <jiamarcos@outlook.com.br>
2022-02-21 14:59:24 -0300

    Primeiro commit

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,27 @@
+import { Component, Output, EventEmitter } from "@angular/core";
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss']
+})
+
+export class NovaTransferenciaComponent {
+
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor: number = 0;
+  destino: number = 0;
+
+  transferir() {
+    console.log('Solicitada nova transferência')
+    this.aoTransferir.emit({ valor: this.valor, destino: this.destino })
+
+    this.limparCampos();
+  }
+
+  limparCampos() {
+    this.valor = 0;
+    this.destino = 0;
+  }
+}
